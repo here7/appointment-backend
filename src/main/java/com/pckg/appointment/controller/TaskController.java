ee31b52835b9dbc53c9dfc4d7a6a4f08cbd8d182
Dani Heredia <info@daniheredia.com>
2019-05-15 20:50:35 +0200

    builder tasks, get task by id and get all of them

--- src/main/java/com/pckg/appointment/controller/TaskController.java
+++ src/main/java/com/pckg/appointment/controller/TaskController.java
@@ -5,49 +5,62 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class TaskController {
-    private Task task = new Task();
+
+    private List<Task> buildTasksList(Integer size){
+        List<Task> listTasks = new ArrayList<>();
+        for (Integer i = 0; i < size; i++){
+            Task task = new Task();
+            task.setId(i);
+            task.setName("Task " + i);
+            task.setDescription("Task Description " + i);
+            task.setDateCreation(new Date());
+            task.setDateTask(new Date());
+            listTasks.add(task);
+        }
+
+        return listTasks;
+    }
 
     @GetMapping("/tasks")
-    public Map<String, Object> getTasks(){
-        HashMap<String, Object> resultTask = new HashMap<>();
+    public Map<Integer,  HashMap<String, Object>> getTasks(){
+        HashMap<Integer,  HashMap<String, Object>> resultMapTask = new HashMap<Integer,  HashMap<String, Object>>();
+        List<Task> listTasks = new ArrayList<>();
+        Integer cont = 0;
 
-        task.setId(1);
-        task.setName("Look trip");
-        task.setDescription("We will check different places to visit");
-        task.setDateCreation(new Date());
-        task.setDateTask(new Date());
+        listTasks = buildTasksList(2);
+        for(Task task : listTasks){
+            HashMap<String, Object> tempMapTask = new HashMap<String, Object>();
+            tempMapTask.put("id",task.getId());
+            tempMapTask.put("name",task.getName());
+            tempMapTask.put("description",task.getDescription());
+            tempMapTask.put("dateCreation",task.getDateCreation());
+            tempMapTask.put("dateTask",task.getDateTask());
+            resultMapTask.put(cont,tempMapTask);
+            cont++;
+        }
 
-        resultTask.put("id",task.getId());
-        resultTask.put("name",task.getName());
-        resultTask.put("description",task.getDescription());
-        resultTask.put("dateCreation",task.getDateCreation());
-        resultTask.put("dateTask",task.getDateTask());
-
-        return resultTask;
+        return resultMapTask;
     }
 
     @GetMapping("/task/{id}")
     public Map<String, Object> getTasks(@PathVariable("id") Integer id){
-        HashMap<String, Object> resultTask = new HashMap<>();
+        HashMap<Integer,  HashMap<String, Object>> tempMapTask = new HashMap<Integer,  HashMap<String, Object>>();
+        HashMap<String, Object> resultTask = new HashMap<String, Object>();
 
-        task.setId(1);
-        task.setName("Look trip");
-        task.setDescription("We will check different places to visit");
-        task.setDateCreation(new Date());
-        task.setDateTask(new Date());
+        List<Task> listTasks = new ArrayList<>();
+        listTasks = buildTasksList(5);
+        Task mytask = new Task();
+        mytask = listTasks.get(id);
 
-        task.setId(2);
-        task.setName("Salesforce Meeting");
-        task.setDescription("Arrive 30min. before");
-        task.setDateCreation(new Date());
-        task.setDateTask(new Date());
-
+        resultTask.put("id",mytask.getId());
+        resultTask.put("name",mytask.getName());
+        resultTask.put("description",mytask.getDescription());
+        resultTask.put("dateCreation",mytask.getDateCreation());
+        resultTask.put("dateTask",mytask.getDateTask());
 
         return resultTask;
     }
